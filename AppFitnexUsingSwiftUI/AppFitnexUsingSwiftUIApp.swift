dc6c260021503caf1ff6585d92d0456b32bc95d8
Jonathan A <jamadorh91@gmail.com>
2023-08-12 18:51:51 -0600

    Initial Commit

---
+++ AppFitnexUsingSwiftUI/AppFitnexUsingSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppFitnexUsingSwiftUIApp.swift
+//  AppFitnexUsingSwiftUI
+//
+//  Created by JonathanA on 12/08/23.
+//
+
+import SwiftUI
+
+@main
+struct AppFitnexUsingSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
